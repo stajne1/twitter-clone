55fa68f0fce9bf53c3fe1132b03a25da5e5dc699
Sandip Tajne <stajne1@gmail.com>
2020-09-09 21:28:04 +0530

    Completed Landing Page

--- src/components/LandingPage/index.js
+++ src/components/LandingPage/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import { Search, Group, ChatBubbleOutline, Twitter }  from '@material-ui/icons';
+import { Search, Group, ChatBubbleOutline, Twitter } from '@material-ui/icons';
+import { Link } from 'react-router-dom';
 import classes from './style.module.css';
 
 const LandingPage = () => {
   return (
     <div className={classes.container}>
+      <div className={classes.rightSection}>
+        <div className={classes.rightSection__content}>
+          <Twitter className={classes.rightSection__brand} />
+          <h1>See what’s happening in the world right now</h1>
+          <h4>Join Twitter today.</h4>
+          <Link className={classes.signupButton} to='/signup'>Sign up</Link>
+          <Link className={classes.loginButton} to='/login' >Log in</Link>
+        </div>
+      </div>
       <div className={classes.leftSection}>
         <Twitter className={classes.leftSection__background} />
         <div className={classes.leftSection__content}>
@@ -13,7 +23,6 @@ const LandingPage = () => {
           <p className={classes.leftSection__content__message}><ChatBubbleOutline /> Join the conversation</p>
         </div>
       </div>
-      <div className={classes.rightSection}>Right Section</div>
     </div>
   );
 }
