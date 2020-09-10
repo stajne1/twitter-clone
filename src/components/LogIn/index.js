82c922da4b4c055d655fd83f8510888168b37dd2
Sandip Tajne <stajne1@gmail.com>
2020-09-10 21:48:58 +0530

    login page design

--- src/components/LogIn/index.js
+++ src/components/LogIn/index.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import { Twitter } from '@material-ui/icons';
+import { Link } from 'react-router-dom';
+import classes from './style.module.css';
 
-const LogIn = props => {
+const LogIn = () => {
   return (
-    <h1>Login Page</h1>
+    <div className={classes.loginContainer}>
+      <Twitter className={classes.twitterLogo} />
+      <h2>Log in to Twitter</h2>
+      <div className={classes.inputContainer}>
+        <label for="username">Phone, email, or username</label>
+        <input type="text" name="username" id="username" />
+      </div>
+      <div className={classes.inputContainer}>
+        <label for="password">Password</label>
+        <input type="password" name="password" id="password" />
+      </div>
+      <button className={classes.loginButton}>Log in</button>
+      <div className={classes.links}>
+        <Link>Forgot Password?</Link> 
+        <span> . </span>
+        <Link>Sign up for Twitter</Link>
+      </div>
+    </div>
   );
 }
 
