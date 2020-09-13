3116681655bf3051a179c4444c5604c868bdfabd
Sandip Tajne <stajne1@gmail.com>
2020-09-13 16:07:13 +0530

    improved design

--- src/components/LogIn/index.js
+++ src/components/LogIn/index.js
@@ -8,15 +8,17 @@ import classes from './style.module.css';
 const LogIn = () => {
   return (
     <div className={classes.loginContainer}>
-      <Twitter className={classes.twitterLogo} />
-      <h2>Log in to Twitter</h2>
-      <Input label="Email" inputProps={{type: 'text', name: 'username', id:'username'}} />
-      <Input label="Password" inputProps={{type: 'password', name: 'password', id:'password'}} />
-      <button className={classes.loginButton}>Log in</button>
-      <div className={classes.links}>
-        <Link to="password-reset">Forgot Password?</Link> 
-        <span> . </span>
-        <Link to="/signup">Sign up for Twitter</Link>
+      <div className={classes.loginForm}>
+        <Twitter className={classes.twitterLogo} />
+        <h2>Log in to Twitter</h2>
+        <Input label="Email" inputProps={{ type: 'text', name: 'username', id: 'username' }} />
+        <Input label="Password" inputProps={{ type: 'password', name: 'password', id: 'password' }} />
+        <button className={classes.loginButton}>Log in</button>
+        <div className={classes.links}>
+          <Link to="password-reset">Forgot Password?</Link>
+          <span> . </span>
+          <Link to="/signup">Sign up for Twitter</Link>
+        </div>
       </div>
     </div>
   );
--- src/components/PasswordReset/index.js
+++ src/components/PasswordReset/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import Input from '../commonComponents/Input';
+import { Twitter } from '@material-ui/icons';
 import classes from './style.module.css';
 
 const PasswordReset = () => {
   return (
     <div className={classes.passwordResetContainer}>
       <div className={classes.form}>
-        <h2>Password reset</h2>
+        <div className={classes.brandContainer}>
+          <Twitter className={classes.twitterLogo} />
+        </div>
         <Input label="Email" inputProps={{ name: 'email', id: 'email', type: 'email' }} />
-        <button>Send Password reset Email</button>
+        <button>Send password reset email</button>
       </div>
     </div>
   );
